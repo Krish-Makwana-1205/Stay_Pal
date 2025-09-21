d92819e4a8858053d743f05a6722cbeb8fb75bcc
Krish-Makwana-1205 <202301103@daiict.ac.in>
2025-09-21 19:24:54 +0530

    User model made

---
+++ server/model/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email:{
+        required:true,
+        unique:true,
+        type:String,
+    },
+    name:{
+        required:true,
+        unique:true,
+        type:String
+    },
+    password:{
+        required:true,
+        type:String
+    }, 
+})
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
