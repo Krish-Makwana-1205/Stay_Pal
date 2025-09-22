a530ddf4a5cb41e0feb8c75c9da209400567001b
Krish-Makwana-1205 <202301103@daiict.ac.in>
2025-09-22 13:26:01 +0530

    User module implemented

---
+++ server/controller/user.js
@@ -0,0 +1,40 @@
+const user = require('../model/user');
+const {setUser, getUser} = require('../service/auth');
+async function makeUser(req, res) {
+    const body = req.body;
+    let using;
+    if((!body.name) || (!body.email) || (!body.password)){
+        return res.redirect('/user/registration')
+    }
+    try {
+        using = await user.create({
+            name: body.name,
+            email: body.email,
+            password: body.password
+        });
+    } catch (error) {
+        return res.redirect('/user/login');
+    }
+    const token = setUser(using);
+    res.cookie('uid', token);
+    return res.redirect('/askQuestion');
+}
+
+async function loginUser(req, res){
+    const body = req.body;
+    if((!body.id) || (!body.password)){
+        return res.redirect('/user/login');
+    }
+    const User = await user.findOne({id:body.id, password:body.password});
+    if(!User){
+        return res.redirect('/user/login');
+    }
+    const token = setUser(User);
+    res.cookie('uid', token);
+    return res.redirect('/home');
+}
+
+module.exports = {
+    makeUser,
+    loginUser
+}
