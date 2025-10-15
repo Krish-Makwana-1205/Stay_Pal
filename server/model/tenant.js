0762af92899800253c5296ca8d718b924ccca0f0
yogesh <202301114@dau.ac.in>
2025-10-15 09:22:46 +0530

    tenant profile

---
+++ server/model/tenant.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+
+const tenantSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    gender: {
+        type: String,
+        enum: ['Male', 'Female', 'Other'],
+        required: true
+    },
+    dob: {
+        type: Date,
+        required: true
+    },
+    foodPreference: {
+        type: String,
+        enum: ['Veg', 'Non-Veg', 'Jain'],
+    },
+    religion: {
+        type: String
+    },
+    alcohol: {
+        type: Boolean,
+
+    },
+    smoker: {
+        type: Boolean,
+    },
+    hometown: {
+        type: String
+    },
+    nationality: {
+        type: String,
+        required: true
+    },
+    nightOwl: {
+        type: Boolean,
+    },
+    hobbies: {
+        type: [String] 
+    },
+    professional_status:{
+        type: String,
+        enum: ['student', 'working']
+
+    },
+    workingshifts: {
+        type: String,
+        enum: ['morning', 'night'],
+ 
+    },    
+    havePet: {
+        type: Boolean,
+    }
+}, { timestamps: true });
+
+const Tenant = mongoose.model('tenant', tenantSchema);
+
+module.exports = Tenant;
