2e4da3543ba39ed8c53ee46f362eaa52d5881556
Meet_Dobariya <202301115@dau.ac.in>
2025-11-14 00:13:47 +0530

    Add Chat model with sender, receiver, and messages schema

---
+++ server/model/chat.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema(
+  {
+    senderEmail: { 
+      type: String,  
+      required: true 
+    },
+    body: { 
+      type: String,   
+      required: true 
+    }
+  },
+  { timestamps: true }  
+);
+
+
+const chatSchema = new mongoose.Schema(
+  {
+    sender: { 
+      type: String,   
+      required: true 
+    },
+    receiver: { 
+      type: String,   
+      required: true 
+    },
+    messages: [messageSchema]  
+  },
+  { timestamps: true }  
+);
+
+chatSchema.index({ sender: 1, receiver: 1 }, { unique: true });
+
+module.exports = mongoose.model("Chat", chatSchema);
